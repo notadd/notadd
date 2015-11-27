2667717c21162913e28830ad5dbabca6871f787e
twilroad <269044570@qq.com>
2015-11-27 23:01:07 +0800

    初始化程序框架……

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2015, iBenchu.org
+ * @datetime 2015-11-27 18:50
+ */
+require '../vendor/autoload.php';
+(new \Notadd\Core(__DIR__ . '/../'))->init()->terminate();

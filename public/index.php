dfba2d64a2d838b3e50174ce2b0ddb7cedf4f4ab
twilroad <269044570@qq.com>
2016-11-21 14:08:51 +0800

    调整public目录下的文件结构

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
 /**
  * This file is part of Notadd.
+ *
  * @author TwilRoad <269044570@qq.com>
- * @copyright (c) 2015, iBenchu.org
- * @datetime 2015-11-27 18:50
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-08-19 22:45
  */
-require '../vendor/autoload.php';
-(new \Notadd\Foundation\Server(__DIR__ . '/../'))->init()->terminate();
+require __DIR__ . '/../storage/bootstraps/autoload.php';
+$application = require_once __DIR__ . '/../storage/bootstraps/application.php';
+$kernel = $application->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle($request = Illuminate\Http\Request::capture());
+$response->send();
+$kernel->terminate($request, $response);

00c7846d70d8814e6d153d9c4723f1cb7e57c8df
twilroad <269044570@qq.com>
2016-11-29 19:17:59 +0800

    创建Application命名空间下，Console模式下的Kernel类，以及Http模式下的Kernel类

---
+++ src/Console/Kernel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-29 19:14
+ */
+namespace Application\Console;
+
+use Notadd\Foundation\Console\Kernel as NotaddKernel;
+
+/**
+ * Class Kernel.
+ */
+class Kernel extends NotaddKernel
+{
+
+}
---
+++ src/Http/Kernel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-29 19:14
+ */
+namespace Application\Http;
+
+use Notadd\Foundation\Http\Kernel as NotaddKernel;
+
+/**
+ * Class Kernel.
+ */
+class Kernel extends NotaddKernel
+{
+
+}

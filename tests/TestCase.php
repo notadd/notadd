1e72e6b5663ecf4a4ed5532a8c0729e13365cd16
twilroad <269044570@qq.com>
2016-11-29 17:00:34 +0800

    移动tests目录……

---
+++ tests/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-10-25 11:58
+ */
+/**
+ * Class TestCase.
+ */
+abstract class TestCase extends Notadd\Foundation\Testing\TestCase
+{
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Notadd\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $application = require __DIR__ . '/../storage/bootstraps/application.php';
+        $application->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $application;
+    }
+}

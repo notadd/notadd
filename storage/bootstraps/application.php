559d14d0e122866611c8d1d0a4d6a5a938326288
twilroad <269044570@qq.com>
2017-02-20 14:11:03 +0800

    remove package notadd/foundation

--- storage/bootstraps/application.php
+++ storage/bootstraps/application.php
@@ -7,8 +7,8 @@
  * @datetime 2016-11-21 11:36
  */
 $application = new \Notadd\Foundation\Application(realpath(__DIR__ . '/../../'));
-$application->singleton(Illuminate\Contracts\Http\Kernel::class, Application\Http\Kernel::class);
-$application->singleton(Illuminate\Contracts\Console\Kernel::class, Application\Console\Kernel::class);
+$application->singleton(Illuminate\Contracts\Http\Kernel::class, Notadd\Foundation\Http\Kernel::class);
+$application->singleton(Illuminate\Contracts\Console\Kernel::class, Notadd\Foundation\Console\Kernel::class);
 $application->singleton(Illuminate\Contracts\Debug\ExceptionHandler::class, Notadd\Foundation\Exception\Handler::class);
 
 return $application;

133be4090ba528b348084d242d9199ae81f459c0
dzzzzzy <insistyao_0522@163.com>
2018-08-07 19:00:43 +0800

    fix(bug): fix resolver method that can not handle the request object

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,24 @@
 import { FastifyAdapter, NestFactory } from '@nestjs/core';
 import { GraphQLFactory } from '@nestjs/graphql';
-import { graphiqlFastify, graphqlFastify } from 'fastify-graphql';
+import * as bodyParser from 'body-parser';
+import { fastifyGraphiQL, fastifyGraphQL } from 'fastify-graphql-middleware';
 
 import { AppModule } from './app.module';
 import { graphqlConfig } from './configurations';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+    if (graphqlConfig.ide.enable) {
+        app.use(graphqlConfig.ide.prefix, fastifyGraphiQL({ endpointURL: graphqlConfig.ide.endpointURL }));
+    }
+
+    app.use('/graphql', bodyParser.json());
+
     const graphQLFactory = app.get(GraphQLFactory);
     const typeDefs = graphQLFactory.mergeTypesByPaths(graphqlConfig.typeDefsPath);
     const schema = graphQLFactory.createSchema({ typeDefs });
-    app.register(graphqlFastify, {
-        prefix: graphqlConfig.prefix,
-        graphql: {
-            schema
-        }
-    });
-    // If graphQL IDE enabled, then register the graphiql Fastify plugin.
-    if (graphqlConfig.ide.enable) {
-        app.register(graphiqlFastify, {
-            prefix: graphqlConfig.ide.prefix,
-            graphiql: {
-                endpointURL: graphqlConfig.ide.endpoint
-            }
-        });
-    }
+    app.use('/graphql', fastifyGraphQL(req => ({ schema, rootValue: req })));
 
     await app.listen(3000);
 }

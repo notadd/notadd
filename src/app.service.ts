aa7b0bb09f845dda714358cad6b1f765f820c4ee
dzzzzzy <insistyao_0522@163.com>
2018-08-22 14:30:36 +0800

    chore(*): remove unused files

--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-
-    async root(): Promise<string> {
-        return 'Hello World.';
-    }
-}
--- src/configurations/graphql.config.ts
+++
@@ -1,9 +0,0 @@
-export const graphqlConfig = {
-    typeDefsPath: './**/*.types.graphql',
-    prefix: '/graphql',
-    ide: {
-        enable: true,
-        prefix: '/graphiql',
-        endpointURL: '/graphql'
-    }
-};

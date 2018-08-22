981845f9772e405071625734fc4105dbb377be54
dzzzzzy <insistyao_0522@163.com>
2018-08-22 14:32:18 +0800

    feat(interceptor): add errors interceptor

---
+++ src/common/interceptors/errors.interceptor.ts
@@ -0,0 +1,21 @@
+import { ExecutionContext, HttpException, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorsInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, calls$: Observable<any>): Observable<any> {
+        return calls$.pipe(catchError((error, caught) => {
+            if (error instanceof HttpException) {
+                return Promise.resolve({
+                    code: error.getStatus(),
+                    message: error.getResponse()
+                });
+            }
+            return Promise.resolve({
+                code: 500,
+                message: `出现了意外错误：${error.toString()}`
+            });
+        }));
+    }
+}

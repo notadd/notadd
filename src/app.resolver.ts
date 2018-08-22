bb736776fb431504d3aba23ff68e8231291943e6
dzzzzzy <insistyao_0522@163.com>
2018-08-22 14:34:33 +0800

    feat(resolver): add rpc method caller

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,16 +1,24 @@
-import { Inject } from '@nestjs/common';
+import { Inject, OnModuleInit } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
 
-import { AppService } from './app.service';
+import { RootServiceInterface } from './app.interface';
+import { NotaddGrpcClientFactory } from './grpc/grpc.client-factory';
 
 @Resolver()
-export class AppResolver {
+export class AppResolver implements OnModuleInit {
+    onModuleInit() {
+        this.rootServiceInterface = this.notaddGrpcClientFactory.rootServiceClient.getService<RootServiceInterface>('RootService');
+    }
+
     constructor(
-        @Inject(AppService) private readonly appService: AppService,
+        @Inject(NotaddGrpcClientFactory) private readonly notaddGrpcClientFactory: NotaddGrpcClientFactory
     ) { }
 
-    @Query('root')
-    async root(): Promise<string> {
-        return this.appService.root();
+    private rootServiceInterface: RootServiceInterface;
+
+    @Query('sayHello')
+    async sayHello(req, body: { name: string }) {
+        const { msg } = await this.rootServiceInterface.sayHello({ name: body.name }).toPromise();
+        return { code: 200, message: 'success', data: msg };
     }
 }

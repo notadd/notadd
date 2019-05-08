157bcbeb507be80ee5bb02e81671c1351eb6032c
zhixilu <321420745@qq.com>
2019-05-08 15:41:57 +0800

    commit

--- apps/nest-upms/src/app.module.ts
+++ apps/nest-upms/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService]
 })

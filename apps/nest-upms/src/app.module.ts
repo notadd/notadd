23268bd2e0c0bc5a230042e7387c047a9adffd85
tangxiaonuo <250148240@qq.com>
2019-05-10 15:48:39 +0800

    typeormentity

--- apps/nest-upms/src/app.module.ts
+++ apps/nest-upms/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserController } from './prisma/user.controller';
 
 @Module({
-  imports: [
-    PrismaModule,
-  ],
-  controllers: [AppController],
+ 
+  imports: [PrismaModule],
+  
+  controllers: [AppController ,UserController],
   providers: [AppService]
 })
 export class ApplicationModule {}

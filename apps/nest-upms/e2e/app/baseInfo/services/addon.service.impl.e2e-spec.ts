afd333cd55017422761ef0d001ea4c742f326695
imeepos <1037483576@qq.com>
2019-05-14 01:41:37 +0800

    fix(sso) test units

--- apps/nest-upms/e2e/app/baseInfo/services/addon.service.impl.e2e-spec.ts
+++ apps/nest-upms/e2e/app/baseInfo/services/addon.service.impl.e2e-spec.ts
@@ -3,7 +3,7 @@ import { Test } from '@nestjs/testing';
 import { AddonEntity } from '../../../../src/typeorm'
 import { ApplicationModule } from '../../../../src'
 import { AddonService } from '../../../../src/baseInfo/core'
-import { AddonNameError, AddonIsNullError } from '../../../../src/baseInfo/errors/error';
+import { AddonNameError, AddonIsNullError, DataError } from '../../../../src/baseInfo/errors/error';
 import { ServerError } from '../../../../src/baseInfo/errors/role.error';
 
 describe('AddonServiceImpl', () => {
@@ -17,11 +17,17 @@ describe('AddonServiceImpl', () => {
         addonService = app.get(AddonService);
 
         /** 添加应用 */
-        let addon: AddonEntity = new AddonEntity();
-        addon.name = 'addon1';
-        addon.title = 'addon1_title';
-        addon.appsecret = '123';
-        addonService.insert(addon).then().catch(e => { });
+        try {
+            let addon: AddonEntity = new AddonEntity();
+            addon.name = 'addon1';
+            addon.title = 'addon1_title';
+            addon.appsecret = '123';
+            addonService.insert(addon).then(res => {
+                // 插入之前要判断 必须要判断
+            }).catch(e => {
+                throw e;
+            });
+        } catch (e) { }
         await app.init();
     });
 
@@ -34,7 +40,7 @@ describe('AddonServiceImpl', () => {
         addonService.insert(addon).then(res => {
             expect(res.name).toEqual(`public addon`)
         }).catch(e => {
-            expect(e instanceof AddonNameError).toBeTruthy();
+            expect(e instanceof AddonNameError).toEqual(true);
         });
         // expect(0).toBe(0);
     });
@@ -42,9 +48,8 @@ describe('AddonServiceImpl', () => {
     /** 更新应用 */
     it(`save`, async () => {
         let newAddon = new AddonEntity();
-        let oldAddon = await addonService.get({ name: '公共应用' })
         newAddon.title = 'new title';
-        addonService.save(newAddon, { appid: oldAddon.appid }).then(res => {
+        addonService.save(newAddon, { name: 'public addon' }).then(res => {
             expect(res.title).toEqual('new title')
         }).catch(e => {
             expect(e instanceof ServerError).toBeTruthy()
@@ -53,8 +58,7 @@ describe('AddonServiceImpl', () => {
 
     /** 删除应用 */
     it(`delete`, async () => {
-        let addon = await addonService.get({ name: '应用一' });
-        addonService.delete(addon).then(res => {
+        addonService.delete({ name: 'addon1' }).then(res => {
             expect(res.affected).toEqual(1)
         }).catch(e => {
             expect(e instanceof AddonIsNullError).toEqual(true)
@@ -63,9 +67,11 @@ describe('AddonServiceImpl', () => {
 
     /** 获取应用 */
     it(`get`, async () => {
-        let addon1 = await addonService.get({ name: '公共应用' })
-        let addon2 = await addonService.get({ name: '公共应用' });
-        expect(addon1).toEqual(addon2);
+        addonService.get({ name: 'public addon' }).then(res => {
+            expect(res.name).toEqual('public addon')
+        }).catch(e => {
+            expect(e instanceof DataError).toEqual(true)
+        });
     });
 
     afterAll(async () => {

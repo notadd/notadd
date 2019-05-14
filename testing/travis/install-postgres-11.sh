dfae85729959569a9dee69db1cca1b399260bc7d
huadong zuo <admin@zuohuadong.cn>
2019-05-14 10:42:17 +0800

    chore(test): fix pg11

--- testing/travis/install-postgres-11.sh
+++ testing/travis/install-postgres-11.sh
@@ -6,7 +6,7 @@ echo "Preparing Postgres 11"
 
 sudo service postgresql stop || true
 
-sudo docker run -d --name postgres11 -p 5432:5432 postgres:11.1
+sudo docker run -d --name postgres11 -p 5432:5432 -e POSTGRES_DB=test_ci POSTGRES_PASSWORD=postgres postgres:11
 sudo docker exec -i postgres11 bash <<< 'until pg_isready -U postgres > /dev/null 2>&1 ; do sleep 1; done'
 
 echo "Postgres 11 ready"

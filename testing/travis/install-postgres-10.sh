5aa69ca61552a489abf7c9e719a2519998fcb7b9
huadong zuo <admin@zuohuadong.cn>
2019-05-14 11:01:14 +0800

    chore(test): fix ming‘s pg10 BUG’

--- testing/travis/install-postgres-10.sh
+++ testing/travis/install-postgres-10.sh
@@ -6,7 +6,7 @@ echo "Preparing Postgres 10"
 
 sudo service postgresql stop || true
 
-sudo docker run -d --name postgres10 -p 5432:5432 -e POSTGRES_DB=test_ci POSTGRES_PASSWORD=postgres postgres:10
+sudo docker run -d --name postgres10 -p 5432:5432 -e POSTGRES_DB=test_ci -e POSTGRES_PASSWORD=postgres postgres:10
 sudo docker exec -i postgres11 bash <<< 'until pg_isready -U postgres > /dev/null 2>&1 ; do sleep 1; done'
 
 echo "Postgres 10 ready"

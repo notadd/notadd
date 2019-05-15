1e8b06669c59d41596e8be0fe7b2a6b6b8801091
tangxiaonuo <250148240@qq.com>
2019-05-15 16:17:34 +0800

    feat(sso)memberservice

--- apps/nest-upms/src/commonServices/index.ts
+++ apps/nest-upms/src/commonServices/index.ts
@@ -1,9 +1,14 @@
-import { ArticleService } from './core';
+import { ArticleService, MemberService } from './core';
 import { ArticleServiceImpl } from './services/article.service.impl';
+import { MemberServiceImpl } from './services/member.service.impl';
 
 export default [{
     provide: ArticleService,
     useClass: ArticleServiceImpl
+},
+{
+    provide: MemberService,
+    useClass: MemberServiceImpl
 },
 
 ]

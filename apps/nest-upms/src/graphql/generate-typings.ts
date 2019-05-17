16ade802d9ffa4280dc3d6bf73651479f8321c2b
imeepos <1037483576@qq.com>
2019-05-17 10:57:51 +0800

    fix(sso) remove old files

--- apps/nest-upms/src/graphql/generate-typings.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
-import { join } from 'path';
-
-const definitionsFactory = new GraphQLDefinitionsFactory();
-definitionsFactory.generate({
-    typePaths: [join(__dirname, '**/*.graphql')],
-    path: join(__dirname, 'src/graphql.schema.ts'),
-    outputAs: 'class',
-});

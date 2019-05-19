29b06541cb884ca47d6b4b3a25e0690724271cdd
imeepos <1037483576@qq.com>
2019-05-19 16:58:02 +0800

    feat(sso) add auto create proto and graphql to package task

--- apps/nest-upms/src/tools/watch.ts
+++ apps/nest-upms/src/tools/watch.ts
@@ -1,9 +1,8 @@
 
 import { join, extname } from 'path';
 import chokidar from 'chokidar';
-import { build } from './build';
-const root = process.cwd();
-
+import { build } from './build'
+const root = process.cwd()
 chokidar.watch(join(root, 'apps/nest-upms/src')).on('all', (opt: string, path: string) => {
     if (path.endsWith('.ts')) {
         const ext = extname(path)

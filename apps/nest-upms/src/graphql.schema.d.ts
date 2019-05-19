2654ff01df3a6f4fbea82391d31a5adeffb35cc9
zhixilu <321420745@qq.com>
2019-05-19 14:06:41 +0800

    feat(sso) add sso resolver

--- apps/nest-upms/src/graphql.schema.d.ts
+++ apps/nest-upms/src/graphql.schema.d.ts
@@ -5,33 +5,39 @@
  */
 
 /* tslint:disable */
-export class LogoutResult {
-    code: number;
-    message?: string;
+export class LogoutBodyRequest {
+    access_token: string;
+}
+
+export class RefreshTokenRequest {
+    access_token: string;
+}
+
+export class TokenBodyRequest {
+    username: string;
+    password: string;
+}
+
+export class VerifyBodyRequest {
+    access_token: string;
 }
 
 export abstract class IMutation {
-    abstract refreshToken(refresh_token: string): RefreshTokenResult | Promise<RefreshTokenResult>;
-    abstract logout(access_token: string): LogoutResult | Promise<LogoutResult>;
+    abstract refreshToken(body?: RefreshTokenRequest): Result | Promise<Result>;
+    abstract logout(body?: LogoutBodyRequest): Result | Promise<Result>;
 }
 
 export abstract class IQuery {
-    abstract token(username: string, password: string): TokenResult | Promise<TokenResult>;
-    abstract verify(access_token: string): UserInfo | Promise<UserInfo>;
+    abstract token(body?: TokenBodyRequest): TokenResult | Promise<TokenResult>;
+    abstract verify(body?: VerifyBodyRequest): Result | Promise<Result>;
 }
 
-export class RefreshTokenResult {
+export class Result {
     code: number;
-    access_token: string;
-    refresh_token: string;
+    msg: string;
 }
 
 export class TokenResult {
     code: number;
-}
-
-export class UserInfo {
-    username: string;
-    avatar: string;
-    nickname: string;
+    msg: string;
 }
--- apps/nest-upms/src/sso/sso.resolver.ts
+++ apps/nest-upms/src/sso/sso.resolver.ts
@@ -2,6 +2,9 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { SsoService } from './core/sso.service';
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AccessTokenEntity } from '../typeorm';
 
 @Controller()
 @Resolver()
@@ -12,9 +15,19 @@ export class SsoResolver {
      */
     @Query()
     @GrpcMethod('SsoService')
-    async token(@Args() body: { username: string, password: string }) {
+    token(@Args() body: TokenBodyRequest): Observable<TokenResult> {
+        // 暂放
         console.log(body.password)
-        return await this.sso.token(body.username, body.password);
+        return from(this.sso.token(body.username, body.password))
+            .pipe(
+                map(res => {
+                    return {
+                        code: 200,
+                        msg: '创建成功',
+                        token: res
+                    }
+                }),
+            );
     }
 
     /**
@@ -22,8 +35,16 @@ export class SsoResolver {
      */
     @Query()
     @GrpcMethod('SsoService')
-    async verify(@Args() body: { access_token: string }) {
-        return await this.sso.verify(body.access_token);
+    verify(@Args() body: VerifyBodyRequest): Observable<Result> {
+        return from(this.sso.verify(body.access_token))
+            .pipe(
+                map(res => {
+                    return {
+                        code: 200,
+                        msg: '验证成功'
+                    }
+                }),
+            );
     }
 
     /**
@@ -31,16 +52,64 @@ export class SsoResolver {
      */
     @Mutation()
     @GrpcMethod('SsoService')
-    refreshToken(@Args() body: { access_token: string }) {
-        return this.sso.refreshToken(body.access_token);
+    refreshToken(@Args() body: RefreshTokenRequest): Observable<Result> {
+        return from(this.sso.refreshToken(body.access_token))
+            .pipe(
+                map(res => {
+                    return {
+                        code: 200,
+                        msg: '刷新成功'
+                    }
+                }),
+            );
     }
+
     /**
      * 注销
      */
     @Mutation()
     @GrpcMethod('SsoService')
-    logout(@Args() body: { access_token: string }) {
-        let token = this.sso.getTokenByAccessToken(body.access_token);
-        return this.sso.logout(body.access_token);
+     logout(@Args() body: LogoutBodyRequest): Result {
+        // let token = await this.sso.getTokenByAccessToken(body.access_token);
+        return {
+            code: 200,
+            msg: '注销成功',
+        }
+        //     return from(this.sso.logout(body.access_token))
+        //         .pipe(
+        //             map(res => {
+        //                 return {
+        //                     code: 200,
+        //                     msg: '注销成功',
+        //                 }
+        //             }),
+        //         );
     }
+}
+
+export interface TokenBodyRequest {
+    username: string;
+    password: string;
+}
+
+export interface LogoutBodyRequest {
+    access_token: string;
+}
+
+export interface VerifyBodyRequest {
+    access_token: string;
+}
+
+export interface RefreshTokenRequest {
+    access_token: string;
+}
+
+export interface TokenResult {
+    code: number;
+    msg: string;
+}
+
+export interface Result {
+    code: number;
+    msg: string;
 }

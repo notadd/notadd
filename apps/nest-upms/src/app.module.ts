45fd776745f33923a9bfa58c15a5b3c24e3b7a3a
imeepos <1037483576@qq.com>
2019-05-22 15:51:10 +0800

    feat(sso) prisma

--- apps/nest-upms/src/app.module.ts
+++ apps/nest-upms/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth';
 import baseInfoProviders from './baseInfo';
 import casbinProviders from './casbin/providers';
@@ -10,7 +9,6 @@ import { GraphqlOptions } from './graphql.options';
 import { ssoProviders } from './sso';
 import { JwtStrategyImpl } from './sso/jwt.strategy.impl';
 import { SsoResolver } from './sso/sso.resolver';
-import allEntities from './typeorm'
 import { InstallResolver } from './install/services/install.service';
 // type: 'postgres',
 // host: 'localhost',
@@ -21,25 +19,9 @@ import { InstallResolver } from './install/services/install.service';
 @Module({
   imports: [
     AuthModule.forRoot(JwtStrategyImpl),
-    TypeOrmModule.forRoot({
-      type: 'cockroachdb',
-      host: '192.168.1.144',
-      port: 26257,
-      username: 'root',
-      password: 'root',
-      database: 'defaultdb',
-      logging: false,
-      entities: [
-        ...allEntities
-      ],
-      synchronize: true,
-    }),
     GraphQLModule.forRootAsync({
       useClass: GraphqlOptions,
-    }),
-    TypeOrmModule.forFeature([
-      ...allEntities
-    ])
+    })
   ],
   controllers: [
     SsoResolver,

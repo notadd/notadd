9c963b9256c2866050b91ba3fd38ffd7e55802f5
imeepos <1037483576@qq.com>
2019-05-30 16:36:22 +0800

    fix(cms)

---
+++ apps/noxus/casbin/lib/index.ts
@@ -0,0 +1,1 @@
+export * from './casbin.module'
--- apps/noxus/cms/lib/cms.module.ts
+++ apps/noxus/cms/lib/cms.module.ts
@@ -1,14 +1,15 @@
 import { MagnusClientModule } from '@notadd/magnus-client'
 import { Module } from '@nestjs/common';
 import { join } from 'path';
-
+import { getConfig } from '@notadd/magnus-client'
+const options = getConfig();
 @Module({
     imports: [
         MagnusClientModule.create({
             inputGolb: join(__dirname, '/**/*.ts'),
             outputPath: __dirname,
             apollo: {
-                uri: 'http://localhost:9000'
+                uri: `http://${options.magnusHost}:${options.magnusPort}`
             }
         })
     ]

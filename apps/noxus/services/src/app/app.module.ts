97f3fd852455455e2fdcf84a95d8b9183da6af40
imeepos <1037483576@qq.com>
2019-05-31 14:01:32 +0800

    fix(dev)

--- apps/noxus/services/src/app/app.module.ts
+++ apps/noxus/services/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MagnusClientModule } from '@magnus/client'
+
 @Module({
-  imports: [],
+  imports: [
+    MagnusClientModule.forRoot(__dirname)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
